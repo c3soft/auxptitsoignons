7842b95c04bb60ccc9d701d114efa435ef0f162c
Pascal <it.tech@laposte.net>
2022-11-21 23:04:30 +0100

    create modal

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const hamburgerButton = document.querySelector('.nav-toggler');
-const navigation = document.querySelector('nav');
+// const hamburgerButton = document.querySelector('.nav-toggler');
+// const navigation = document.querySelector('nav');
 
-hamburgerButton.addEventListener("click", toggleNav);
+// hamburgerButton.addEventListener("click", toggleNav);
 
 function toggleNav() {
     hamburgerButton.classList.toggle("active");
     navigation.classList.toggle("active");
+}
+
+const modalContainer = document.querySelector(".modal-container");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
+
+modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal));
+
+function toggleModal() {
+    modalContainer.classList.toggle("active")
 }
